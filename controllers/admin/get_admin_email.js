ce2a134a3ea8e6adf2e97b1c54660911a213c29c
MartynLCD3 <martincastagno@protonmail.com>
2021-05-06 17:41:25 -0300

    configuracion adicional

--- controllers/admin/get_admin_email.js
+++ controllers/admin/get_admin_email.js
@@ -3,7 +3,6 @@ export const get_admin_email = (res) => {
   Promise.all([read_email()]).then(objects => {
     let [email] = objects;
     if(email.length == 0) return res.status(404).json({msg:0});
-    res.status(200).json({msg:"ok"});
-    return email;
+    res.status(200).json(email);
   });
 }
--- controllers/emails/get_emails.js
+++ controllers/emails/get_emails.js
@@ -2,8 +2,8 @@ import {read_emails_format} from '../../models/emails/read_emails_format.js';
 export const get_emails = (res) => {
   Promise.all([read_emails_format()]).then(objects => {
     let [emails] = objects;
-    if(emails.length == 0) return res.status(404).json({msg:0});
-    res.status(200).json({msg:"ok"});
-    return emails.reverse();
+    if(emails.length == 0) return res.status(200);
+    let list = emails.reverse()
+    res.status(200).json(list);
   });
 }
